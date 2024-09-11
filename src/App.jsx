973366d2878ebe57824efe8c374db4b8c2e4e486
RyneeeJ <imrynegandia@gmail.com>
2024-09-11 23:49:33 +0800

    Set up supabase client and tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,35 @@ import Profile from "./pages/Profile";
 import PaymentMethod from "./pages/PaymentMethod";
 import PurchaseHistory from "./pages/PurchaseHistory";
 import PageNotFound from "./pages/PageNotFound";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index path="/" element={<Homepage />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/account/cart" element={<Cart />} />
-          <Route path="/account/profile" element={<Profile />} />
-          <Route
-            path="/account/profile/payment-method"
-            element={<PaymentMethod />}
-          />
-          <Route
-            path="/account/profile/purchase-history"
-            element={<PurchaseHistory />}
-          />
-          <Route path="/login" element={<Login />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index path="/" element={<Homepage />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/account/cart" element={<Cart />} />
+            <Route path="/account/profile" element={<Profile />} />
+            <Route
+              path="/account/profile/payment-method"
+              element={<PaymentMethod />}
+            />
+            <Route
+              path="/account/profile/purchase-history"
+              element={<PurchaseHistory />}
+            />
+            <Route path="/login" element={<Login />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
