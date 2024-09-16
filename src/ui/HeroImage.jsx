58a0711b5c6909a3cf15eb310bbab4749bf9cd6f
RyneeeJ <imrynegandia@gmail.com>
2024-09-16 16:39:09 +0800

    Changed the max-width property of the hero image

--- src/ui/HeroImage.jsx
+++ src/ui/HeroImage.jsx
@@ -1,8 +1,9 @@
 import Button from "./Button";
+import Section from "./Section";
 
 function HeroImage() {
   return (
-    <div className="relative z-10 h-[38rem] w-full bg-[url('/hero-img.jpg')] bg-cover bg-center px-8 py-10 before:absolute before:inset-0 before:-z-20 before:block before:bg-gradient-to-b before:from-yellow-700 before:to-neutral-300 before:opacity-30 before:content-[''] xs:h-[45rem] xs:px-10 sm:px-16 md:h-[55rem] md:px-28">
+    <div className="relative z-10 mx-auto h-[38rem] max-w-6xl bg-[url('/hero-img.jpg')] bg-cover bg-center px-8 py-10 before:absolute before:inset-0 before:-z-20 before:block before:bg-gradient-to-b before:from-yellow-700 before:to-neutral-300 before:opacity-30 before:content-[''] xs:h-[45rem] xs:px-10 sm:px-16 md:h-[55rem] md:px-28">
       <h1 className="mt-10 text-3xl font-light text-amber-50 xs:text-4xl sm:text-5xl md:mt-20">
         Effortless elegance, <br /> purely minimal
       </h1>
