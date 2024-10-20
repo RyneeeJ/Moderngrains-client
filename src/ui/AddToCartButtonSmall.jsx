05bf4161a88817f2ac9b1a396c9028664a713531
RyneeeJ <imrynegandia@gmail.com>
2024-10-20 22:00:30 +0800

    Improved small add to cart button design in product list + changed bestseller product tag color from green to brown

--- src/ui/AddToCartButtonSmall.jsx
+++ src/ui/AddToCartButtonSmall.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
-import { PiShoppingCartSimpleBold } from "react-icons/pi";
+import {
+  PiSealWarning,
+  PiSealWarningBold,
+  PiShoppingCartSimpleBold,
+} from "react-icons/pi";
 import { useCartItems } from "../features/cart/useCartItems";
 import { useAddToCart } from "../features/cart/useAddToCart";
+import toast from "react-hot-toast";
 
 function AddToCartButtonSmall({ item, isOutOfStock }) {
   const [isProcessing, setIsProcessing] = useState(false);
@@ -11,6 +16,7 @@ function AddToCartButtonSmall({ item, isOutOfStock }) {
 
   const { name, id: productId, stripeId } = item;
 
+  const iconClass = "mx-auto size-3.5 xs:size-5 md:size-6 ";
   function handleAddToCart() {
     if (isOutOfStock) {
       toast.error(`${name} is currently out of stock`);
@@ -37,9 +43,13 @@ function AddToCartButtonSmall({ item, isOutOfStock }) {
     <button
       onClick={handleAddToCart}
       disabled={isProcessing}
-      className={`w-1/3 rounded-md bg-stone-800 py-1 transition-all duration-200 hover:bg-stone-700 sm:rounded-lg ${isProcessing && "cursor-not-allowed"}`}
+      className={`w-1/3 rounded-md py-1 transition-all duration-200 sm:rounded-lg ${isProcessing && "cursor-not-allowed"} ${isOutOfStock ? "bg-red-600 hover:bg-red-700" : "bg-lime-800 hover:bg-lime-900"}`}
     >
-      <PiShoppingCartSimpleBold className="mx-auto size-3.5 text-amber-50 xs:size-5 md:size-6" />
+      {isOutOfStock ? (
+        <PiSealWarningBold className={iconClass + "text-red-100"} />
+      ) : (
+        <PiShoppingCartSimpleBold className={iconClass + "text-amber-50"} />
+      )}
     </button>
   );
 }
